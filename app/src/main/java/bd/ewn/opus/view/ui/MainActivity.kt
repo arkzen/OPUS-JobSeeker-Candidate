a40ae7c742e044618ca806c8a19075a0d23f5ce0
Asikur <asikurr817@gmail.com>
2023-08-03 13:48:34 +0600

    Project Initialized

---
+++ app/src/main/java/bd/ewn/opus/view/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package bd.ewn.opus.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bd.ewn.opus.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
