f15ac64d9eed5687cd87de13af04c0a8ef46aa08
Asikur <asikurr817@gmail.com>
2023-08-16 11:48:17 +0600

    Headers added to get method

--- app/src/main/java/bd/ewn/opus/service/network/ApiServices.kt
+++ app/src/main/java/bd/ewn/opus/service/network/ApiServices.kt
@@ -3,9 +3,13 @@ package bd.ewn.opus.service.network
 import bd.ewn.opus.service.model.LoginPojo
 import bd.ewn.opus.service.network.request.LoginBody
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiServices {
     @POST("/user/login/")
-    fun getUser(@Body requestBody: LoginBody) : retrofit2.Call<LoginPojo>
+    fun getUser(@Header("client-id") clientId: String,
+                @Header("client-secret") clientSecret: String,
+                @Header("grant-type") grantType: String,
+                @Body requestBody: LoginBody) : retrofit2.Call<LoginPojo>
 }
