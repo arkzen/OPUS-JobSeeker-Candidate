89ebcdfc39d467d6894d32b6ecc342627d9a561b
Asikur <asikurr817@gmail.com>
2023-08-16 12:07:18 +0600

    Headers added

--- app/src/main/java/bd/ewn/opus/service/repository/OpusRepoImpl.kt
+++ app/src/main/java/bd/ewn/opus/service/repository/OpusRepoImpl.kt
@@ -1,7 +1,6 @@
 package bd.ewn.opus.service.repository
 
 import android.content.Context
-import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import bd.ewn.opus.service.model.LoginPojo
@@ -15,7 +14,7 @@ import retrofit2.Response
 class OpusRepoImpl private constructor() : OpusRepository {
 
     private val mLivedata: MutableLiveData<LoginPojo> = MutableLiveData()
-    private  var rInstance:RetrofitInstance = RetrofitInstance()
+    private var rInstance: RetrofitInstance = RetrofitInstance()
 
     override fun cLoginResponse(
         context: Context,
@@ -23,9 +22,14 @@ class OpusRepoImpl private constructor() : OpusRepository {
         password: String
     ): MutableLiveData<LoginPojo> {
 
-        val apiServices=rInstance.getRetrofitInstance()?.create(ApiServices::class.java)
+        val apiServices = rInstance.getRetrofitInstance()?.create(ApiServices::class.java)
         val requestBody = LoginBody(username, password)
-        val call = apiServices?.getUser(requestBody)
+
+        val clientId = "Wqp1QNVZQdpWuM3xGiegciX8HRgbRCgT7WujzJV7"
+        val clientSecret =
+            "OVTBUwzNChgvmtfRjFNlM29WZboeRXqQfYTLoLy4dQETSpsz7yB9WOW0S01dwXuuwkatVDbJHMKK03RnyjCjjjbU64xBnc0U3oTS8LU1kfHp7kRgFxawTY5YC1mj4UA3"
+        val grantType = "password"
+        val call = apiServices?.getUser(clientId, clientSecret, grantType, requestBody)
 
         try {
             call?.enqueue(object : Callback<LoginPojo> {
@@ -34,24 +38,20 @@ class OpusRepoImpl private constructor() : OpusRepository {
                     response: Response<LoginPojo>
                 ) {
                     if (!response.isSuccessful) {
-                        val errorMessage = response.message()
-                        Log.d("EWN2745", errorMessage)
-                        Toast.makeText(context, "Login Failed: $errorMessage", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(context, "Login Failed", Toast.LENGTH_SHORT).show()
                         return
                     }
                     val loginResponse = response.body()
-
-                    Log.d("EWN2745", loginResponse!!.access_token)
                     mLivedata.postValue(loginResponse!!)
                 }
 
                 override fun onFailure(call: Call<LoginPojo>, t: Throwable) {
-                    Toast.makeText(context, "An Error Occurred, Try Later!!", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, "Login Failed", Toast.LENGTH_SHORT).show()
                 }
             })
         } catch (e: Exception) {
             e.printStackTrace()
-            Toast.makeText(context, "An Error Occurred, Try Later!!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Login Failed", Toast.LENGTH_SHORT).show()
         }
         return mLivedata
     }
