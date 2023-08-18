6b30bf1d6bc9098d64f265f47a0008b80f8ef30c
Asikur <asikurr817@gmail.com>
2023-08-18 21:31:58 +0600

    Navigation from loginpage to homepage added in navgraph

--- app/src/main/java/bd/ewn/opus/view/ui/fragment/auth/LoginFragment.kt
+++ app/src/main/java/bd/ewn/opus/view/ui/fragment/auth/LoginFragment.kt
@@ -1,25 +1,40 @@
 package bd.ewn.opus.view.ui.fragment.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import bd.ewn.opus.R
+import bd.ewn.opus.databinding.FragmentLoginBinding
+import bd.ewn.opus.viewmodel.LoginViewModel
 
-class LoginFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
+class LoginFragment : Fragment(), OnClickListener {
+
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnclicklistener()
+    }
+
+    private fun setOnclicklistener() {
+        binding.btnLogin.setOnClickListener(this)
     }
 
     companion object {
@@ -31,5 +46,55 @@ class LoginFragment : Fragment() {
 
                 }
             }
+    }
+
+    override fun onClick(v: View) {
+
+        when (v.id) {
+            R.id.btnLogin -> {
+                val email = binding.edEmail.text.toString()
+                val pass = binding.edPassword.text.toString()
+
+                if (isValid()) {
+                    loginCheck(email, pass)
+                }
+            }
+
+        }
+    }
+
+    private fun loginCheck(email: String, pass: String) {
+        val loginViewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+
+        loginViewModel.loginResponse(requireContext(), email, pass)
+            .observe(viewLifecycleOwner, Observer { loginPojo ->
+
+                val accessToken = loginPojo.access_token
+                if (accessToken.isNotEmpty()) {
+                    Navigation.findNavController(requireView()).navigate(R.id.homepage)
+                } else Toast.makeText(requireContext(), "Failed", Toast.LENGTH_SHORT).show()
+            })
+    }
+
+    private fun isValid(): Boolean {
+
+        binding.edEmail.error = null
+        binding.edPassword.error = null
+
+        var isValid = true
+        val email = binding.edEmail.toString()
+        val pass = binding.edPassword.toString()
+
+        if (email.isEmpty()) {
+            isValid = false
+            binding.edEmail.error = "Enter Your Email"
+        }
+
+        if (pass.isEmpty()) {
+            isValid = false
+            binding.edPassword.error = "Enter Your Password"
+        }
+
+        return isValid
     }
 }
