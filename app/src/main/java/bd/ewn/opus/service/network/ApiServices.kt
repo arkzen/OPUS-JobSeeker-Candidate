7bc6d8285851ebe2cdba6463becd7fc1990e19ab
Asikur <asikurr817@gmail.com>
2023-08-20 13:37:58 +0600

    getOTPbyEmail method added

--- app/src/main/java/bd/ewn/opus/service/network/ApiServices.kt
+++ app/src/main/java/bd/ewn/opus/service/network/ApiServices.kt
@@ -1,15 +1,22 @@
 package bd.ewn.opus.service.network
 
 import bd.ewn.opus.service.model.LoginPojo
+import bd.ewn.opus.service.model.PassResetEmailPojo
 import bd.ewn.opus.service.network.request.LoginBody
+import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiServices {
     @POST("/user/login/")
-    fun getUser(@Header("client-id") clientId: String,
-                @Header("client-secret") clientSecret: String,
-                @Header("grant-type") grantType: String,
-                @Body requestBody: LoginBody) : retrofit2.Call<LoginPojo>
+    fun getUser(
+        @Header("client-id") clientId: String,
+        @Header("client-secret") clientSecret: String,
+        @Header("grant-type") grantType: String,
+        @Body requestBody: LoginBody
+    ): retrofit2.Call<LoginPojo>
+
+    @POST("/user/password_reset_request/")
+    fun getOTPbyEmail(@Body username: String): Call<PassResetEmailPojo>
 }
