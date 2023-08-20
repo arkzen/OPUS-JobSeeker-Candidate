5aac02014e0024901ff65c7a9fa0bf199523f872
Asikur <asikurr817@gmail.com>
2023-08-20 13:35:15 +0600

    sharedpref updated for storing resetpass email,otp

--- app/src/main/java/bd/ewn/opus/service/util/SharedPrefDataProvider.kt
+++ app/src/main/java/bd/ewn/opus/service/util/SharedPrefDataProvider.kt
@@ -7,6 +7,8 @@ object SharedPrefDataProvider {
     private const val PREF_NAME = "OpusPrefs"
     private const val KEY_LOGIN_STATE = "loginState"
     private const val KEY_ACCESS_TOKEN = "accessToken"
+    private const val KEY_RESETPASS_EMAIL = "resetPassEmail"
+    private const val KEY_RESETPASS_OTP = "resetPassOtp"
 
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var editor: SharedPreferences.Editor
@@ -14,6 +16,24 @@ object SharedPrefDataProvider {
     fun initialize(context: Context) {
         sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
         editor = sharedPreferences.edit()
+    }
+
+    fun setResetPassEmail(resetPassEmail: String) {
+        editor.putString(KEY_RESETPASS_EMAIL, resetPassEmail)
+        editor.apply()
+    }
+
+    fun getResetPassEmail(): String? {
+        return sharedPreferences.getString(KEY_RESETPASS_EMAIL, null)
+    }
+
+    fun setResetPassOtp(resetPassOtp: String) {
+        editor.putString(KEY_RESETPASS_OTP, resetPassOtp)
+        editor.apply()
+    }
+
+    fun getResetPassOtp(): String? {
+        return sharedPreferences.getString(KEY_RESETPASS_OTP, null)
     }
 
     fun setLoginState(loginState: Boolean) {
