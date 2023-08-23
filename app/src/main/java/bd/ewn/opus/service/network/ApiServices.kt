8c3e7f510a52b3898ab7b6dd65e870680c5e8c27
Asikur <asikurr817@gmail.com>
2023-08-23 18:03:44 +0600

    apiservice updated

--- app/src/main/java/bd/ewn/opus/service/network/ApiServices.kt
+++ app/src/main/java/bd/ewn/opus/service/network/ApiServices.kt
@@ -2,21 +2,32 @@ package bd.ewn.opus.service.network
 
 import bd.ewn.opus.service.model.LoginPojo
 import bd.ewn.opus.service.model.PassResetEmailPojo
+import bd.ewn.opus.service.model.SignUpPojo
 import bd.ewn.opus.service.network.request.LoginBody
+import bd.ewn.opus.service.network.request.ResetPassBody
+import bd.ewn.opus.service.network.request.SignupBody
+import bd.ewn.opus.service.network.request.resetReqBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiServices {
+
+    @POST("/user/signup/")
+    fun postSignUp(@Body signupBody:SignupBody):Call<SignUpPojo>
+
     @POST("/user/login/")
     fun getUser(
         @Header("client-id") clientId: String,
         @Header("client-secret") clientSecret: String,
         @Header("grant-type") grantType: String,
         @Body requestBody: LoginBody
-    ): retrofit2.Call<LoginPojo>
+    ): Call<LoginPojo>
 
     @POST("/user/password_reset_request/")
-    fun getOTPbyEmail(@Body username: String): Call<PassResetEmailPojo>
+    fun getOTPbyEmail(@Body emailbody: resetReqBody): Call<PassResetEmailPojo>
+
+    @POST("/user/password_reset/")
+    fun resetPass(@Body requestBody:ResetPassBody):Call<PassResetEmailPojo>
 }
